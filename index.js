509325863b460074c4f98200eb573a210d389a18
AlexHamn <agalexandro16@gmail.com>
2021-09-11 00:16:21 -0500

    add calculator object and test

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function reverseString(string) {
     return (string === '') ? '' : reverseString(string.substr(1)) + string.charAt(0);
 }
 
-module.exports = stringLength;
+module.exports = stringLength, reverseString;
 
 console.log(reverseString("hello"));
